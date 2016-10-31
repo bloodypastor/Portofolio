265fd37453b29d0b29f0fae483d41c2f07f95560
bloodypastor <bloodypastor7@gmail.com>
2016-10-31 13:38:12 +0200

    add php file

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
+<?php
+	if (empty($_POST) === false) {
+		$errors = array();
 
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+
+		if (empty($name) === true || empty($email) === true || empty($message) === true) {
+			$errors[] = 'Name, mail and message are required!';
+		} else {
+			if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+				$errors[] = "That's not a valid email address!";
+			}
+			if (ctype_alpha($name) === false) {
+				$errors[] = 'Name must only contain letters!';
+			}
+		}
+
+		if (empty($errors) === true) {
+			mail('bloodypastor7@gmail.com', 'Contact form', $message, 'Form: ' . $email);
+			header('Location: index.php?sent');
+			exit();
+		}
+
+	}
+?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -16,7 +42,7 @@
 <link href='//fonts.googleapis.com/css?family=Quicksand:400,700,300' rel='stylesheet' type='text/css'>
 <link href='//fonts.googleapis.com/css?family=Cinzel:400,700,900' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Proza+Libre:500" rel="stylesheet">
-<!-- /font links -->	
+<!-- /font links -->
 <!-- js files -->
 <script src="js/modernizr.custom.js"></script>
 <!-- /js files -->
@@ -65,7 +91,7 @@
 					<a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
 					<a href=""><i class="fa fa-github" aria-hidden="true"></i></a>
 				</div>
-			</div>	
+			</div>
 		</li>
 		<li>
 			<div class="banner-info">
@@ -97,11 +123,11 @@
 				</div>
 			</div>
 		</li>
-	</ul>	
+	</ul>
 </div>
 <!-- /Banner -->
 <!-- About -->
-<div class="about-me" id="about"> 
+<div class="about-me" id="about">
 	<h3 class="text-center slideanim">About my skills</h3>
 	<div class="container">
 		<div class="row">
@@ -123,7 +149,7 @@
 				<div class="myskills slideanim">
 					<h3 class="text-center">My Skill Info</h3>
 					<p class="skill-text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-					<div class="skill-info"> 
+					<div class="skill-info">
 						<div class="table-responsive">
 							<table class="table">
 								<tbody>
@@ -154,12 +180,12 @@
 									</tr>
 								</tbody>
 							</table>
-						</div>	
+						</div>
 					</div>
-				</div>	
+				</div>
 			</div>
-		</div>	
-	</div>	
+		</div>
+	</div>
 </div>
 <!-- /About -->
 <!-- Portfolio -->
@@ -230,13 +256,13 @@
 					<span class="icon close-content"></span>
 				</div>
 			</section>
-		</div>	
+		</div>
 <!-- /Portfolio -->
 <!-- Gallery -->
 <div class="mygallery" id="gallery">
 	<h3 class="text-center slideanim">My Gallery</h3>
-	<div class="text-center"> 
-		<a href="images/me1-1.jpg" title="My Image Gallery"><img src="images/me1.jpg" alt="example-1" class="img-responsive slideanim"></a> 
+	<div class="text-center">
+		<a href="images/me1-1.jpg" title="My Image Gallery"><img src="images/me1.jpg" alt="example-1" class="img-responsive slideanim"></a>
 		<a href="images/me3-3.jpg" title="My Image Gallery"><img src="images/me3.jpg" alt="example-1" class="img-responsive slideanim"></a>
 		<a href="images/me2-2.jpg" title="My Image Gallery"><img src="images/me2.jpg" alt="example-2" class="img-responsive slideanim"></a>
 		<a href="images/me4-4.jpg" title="My Image Gallery"><img src="images/me4.jpg" alt="example-2" class="img-responsive slideanim"></a>
@@ -250,9 +276,9 @@
 
 	</div>
 
-</div>	
+</div>
 <!-- Gallery -->
-<!-- footer -->	
+<!-- footer -->
 <div class="contact-me" id="contact">
 	<h3 class="text-center slideanim">Contact Me</h3>
 	<!--<p class="text-center slideanim">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>-->
@@ -261,24 +287,24 @@
 		<div class="row">
 			<div class="col-md-6 slideanim">
 				<div class="contact-details">
-					<form action="#" method="post">
-						<div class="row">
-							<div class="col-sm-12 form-group">
-								<input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
-							</div>
-							<div class="col-sm-12 form-group">
-								<input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-							</div>
+				<form action="#" method="post">
+					<div class="row">
+						<div class="col-sm-12 form-group">
+							<input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
 						</div>
-						<textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea><br>
-						<div class="row">
-							<div class="col-sm-12 form-group">
-								<button class="btn btn-default btn-lg" type="submit">Send <span class="glyphicon glyphicon-menu-right"></span></button>
-							</div>
+						<div class="col-sm-12 form-group">
+							<input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
 						</div>
-					</form>	
-				</div>	
-			</div>	
+					</div>
+					<textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea><br>
+					<div class="row">
+						<div class="col-sm-12 form-group">
+							<button class="btn btn-default btn-lg" type="submit">Send <span class="glyphicon glyphicon-menu-right"></span></button>
+						</div>
+					</div>
+				</form>
+				</div>
+			</div>
 			<div class="col-md-6 slideanim">
 				<div class="contact-info">
 					<h4>Contact Info</h4>
@@ -315,7 +341,7 @@
 		</footer>
 	</div>
 </div>
-<!-- /footer -->	
+<!-- /footer -->
 	<!-- js files -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
  	<!--<script src="js/jquery-3.1.1.min.js"></script>-->
@@ -323,10 +349,10 @@
 	<!-- js files for banner slider -->
 	<script src="js/responsiveslides.min.js"></script>
 	  <script>
-    $(window).load(function() {
+$(window).load(function() {
 
-     // Slideshow for banner
-      $("#slider").responsiveSlides({
+    // Slideshow for banner
+    $("#slider").responsiveSlides({
         maxwidth: 1920,
         speed: 1000
       });
@@ -340,52 +366,52 @@
 		<script src="js/helper.js"></script>
 		<script src="js/grid3d.js"></script>
 		<script>
-			new grid3D( document.getElementById( 'grid3d' ) );
+new grid3D( document.getElementById( 'grid3d' ) );
 		</script>
 	<!-- /js files for portfolio -->
 	<!-- js files for gallery -->
 <script type="text/javascript" src="js/cobox.js"></script>
-	<!-- /js files for gallery -->	
+	<!-- /js files for gallery -->
 	<!-- js for smooth scrolling -->
-	
+
 		<script>
 $(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-  // Store hash
-  var hash = this.hash;
+        // Store hash
+        var hash = this.hash;
 
-  // Using jQuery's animate() method to add smooth page scroll
-  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-  $('html, body').animate({
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
     scrollTop: $(hash).offset().top
   }, 900, function(){
 
-    // Add hash (#) to URL when done scrolling (default click behavior)
-    window.location.hash = hash;
-    });
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
   });
 })
 
 </script>
 	<!-- /js for smooth scrolling -->
 	<!-- js for sliding -->
-	
-	<script>
-	$(window).scroll(function() {
-  $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
 
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      $(this).addClass("slide");
-    }
-  });
+	<script>
+$(window).scroll(function() {
+    $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+            $(this).addClass("slide");
+        }
+    });
 });
 </script>
 	<!-- /js for sliding -->
 	<!-- /js files -->
-	
+
 </body>
-</html>	
+</html>
